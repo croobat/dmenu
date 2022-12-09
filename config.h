ff87a08b317c76f3925cca065b2b6f968c2df667
Tony Ramírez <tonyramirez.business@outlook.com>
2022-12-09 09:11:02 -0600

    Added extra fonts and some lines

--- config.h
+++ config.h
@@ -4,7 +4,9 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Recursive Mono Linear Static:size=11:style=Medium"
+	"Recursive Mono Linear Static:size=11:style=Medium:antialias=true:autohint=true",
+	"Hack Nerd Font:pixelsize=11:antialias=true:autohint=true",
+	"Symbola:pixelsize=11:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
@@ -27,7 +29,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { col_black, col_cyan },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
