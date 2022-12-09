38e8fa3601c4f34a3061652617587f069505da78
Tony Ramírez <tonyramirez.business@outlook.com>
2022-12-09 08:36:40 -0600

    Bar in bottom and font recursive

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono Nerd Font:size=12:style=Medium"
+	"Recursive Mono Linear Static:size=11:style=Medium"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
