829da1dea89d352a37c86f4583b964df849500c0
Tony Ramírez <tonyramirez.business@outlook.com>
2024-02-09 14:30:54 -0600

    feat: change min width

--- config.def.h
+++ config.def.h
@@ -2,9 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 768;                 /* minimum width when centered */
+static int min_width = 640;                 /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Recursive Mono Linear Static:size=10:style=Medium:antialias=true:autohint=true",
